7e2e9e6b1e32bfb28f2c61a16ee841ffb3275320
BlakeWewer <you@example.com>
2018-02-14 19:50:15 -0600

    Need to work on Mem Images

--- CIS 450/Assignment 2/Wewer_Blake/question3/question3.c
+++ CIS 450/Assignment 2/Wewer_Blake/question3/question3.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
 int i = malloc(sizeof(int));
-char c1 = malloc(sizeof(char));
+char *c1 = malloc(sizeof(char));
 
 int j = 3;
 int m = 1;
 
-//string
-string [NAME] = "\n%d\t%d\t%d\t%d\t%d\t%d\n";
-// DB : Lines 186 - 188 ?????
 
-trace_me(int param1, int param2, char uc)
+int trace_me(int param1, int param2, char uc)
 {
-  
+	static int p;
+	static int o;
+	int n;
+	n = 3;
+	m++;
+	
+	if(param1 == 0)
+	{
+		p = m;
+		o = param2;
+		n = param1;
+		char NAME[] = "\n%d\t%d\t%d\t%d\t%d\t%d\n";
+		printf(NAME, p, o, n);
+	}else{
+		o++;
+		p++;
+		param2++;
+		c1++;
+		trace_me(param1 - 1, param2);
+	}
+	
+	return 0;
 }
