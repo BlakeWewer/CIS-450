2f7bb0430f72a2947df5fbf4ab362ae5f1f0ec39
BlakeWewer <you@example.com>
2018-03-02 11:02:42 -0600

    Modified Memory Images
    Signed-off-by: BlakeWewer <you@example.com>

--- CIS 450/Assignment 3/Wewer_Blake/Question1/question1.s
+++ CIS 450/Assignment 3/Wewer_Blake/Question1/question1.s
@@ -4,7 +4,9 @@
 	.align 4
 _i1:
 	.long	5
-.lcomm _i2,4,4
+.lcomm _i2,4,4			;	allocate 4 bytes at 4-byte boundary
+						;	and name the area _i2
+						;	the name is not exported
 .globl _s1
 	.align 2
 _s1:
@@ -17,6 +19,9 @@ LC0:
 .globl _func
 	.def	_func;	.scl	2;	.type	32;	.endef
 _func:
+
+;		void func(int fp1, int fp2, int fp3)
+;		{
 	pushl	%ebp
 	movl	%esp, %ebp
 	pushl	%ebx
@@ -32,10 +37,10 @@ _func:
 	movzwl	_s1, %eax
 	movswl	%ax, %edx
 	movl	_i1, %eax
-	cmpl	%eax, %edx
-	jge	L2
+	cmpl	%eax, %edx		;	Compares %eax to %edx
+	jge	L2					;	Jump to L2 if %eax is greater than or equal to %edx
 	
-	jmp	L3
+	jmp	L3					;	L3 is the beginning of the else
 ;		}
 	
 L4:
@@ -45,17 +50,17 @@ L4:
 	
 ;				f1 = s1 + fp1; 
 	movzwl	_s1, %eax
-	leal	(%edx,%eax), %eax
-	movw	%ax, -10(%ebp)
+	leal	(%edx,%eax), %eax		; %eax <- (%edx + %eax)
+	movw	%ax, -10(%ebp)			; 10(%ebp): f1
 	
 ;				f2 = fp2 - s1;
-	movl	12(%ebp), %eax
+	movl	12(%ebp), %eax			; 12(%ebp): fp2
 	movl	%eax, %edx
 	movzwl	_s1, %eax
 	movl	%edx, %ecx
 	subw	%ax, %cx
 	movl	%ecx, %eax
-	movw	%ax, -12(%ebp)
+	movw	%ax, -12(%ebp)			; -12(%ebp): f2
 	
 ;				i2 + i2 + 1;
 	movl	_i2, %eax
@@ -63,22 +68,22 @@ L4:
 	movl	%eax, _i2
 
 
-L3:
+L3:									; Beginning of if(s1 < i1) is true block
 
 ;			while(i2 < 1)
 ;			{
 	movl	_i2, %eax
 	cmpl	$1, %eax
-	jle	L4
+	jle	L4							; Jump L4 if 1 is less than or equal to i2
 ;			}
 	
 ;			f3 = f2 + f1 + fp3;
-	movswl	-10(%ebp), %eax
+	movswl	-10(%ebp), %eax			; -10(%ebp): f1
 	movl	%eax, %edx
-	addl	16(%ebp), %edx
-	movzwl	-12(%ebp), %eax
+	addl	16(%ebp), %edx			; 16(%ebp): fp1
+	movzwl	-12(%ebp), %eax			; -12(%ebp): f2
 	leal	(%edx,%eax), %eax
-	movl	%eax, -16(%ebp)
+	movl	%eax, -16(%ebp)			; -16(%ebp): f3
 
 ;			s2 + s2 + 1;	
 	movzwl	_s2, %eax
@@ -86,19 +91,19 @@ L3:
 	movw	%ax, _s2
 	
 	func(f1, f2, f3);
-	movzwl	-12(%ebp), %edx
-	movswl	-10(%ebp), %eax
-	movl	-16(%ebp), %ecx
-	movl	%ecx, 8(%esp)
-	movl	%edx, 4(%esp)
-	movl	%eax, (%esp)
+	movzwl	-12(%ebp), %edx			; -12(%ebp): f2
+	movswl	-10(%ebp), %eax			; -10(%ebp): f1
+	movl	-16(%ebp), %ecx			; -16(%ebp): f3
+	movl	%ecx, 8(%esp)			; push f3
+	movl	%edx, 4(%esp)			; push zero extended f2
+	movl	%eax, (%esp)			; push sign extended f1
 	call	_func
 	
 ;			}
-	jmp	L1
+	jmp	L1							; End of if(s1 < i1) is true block
 
 
-L2:
+L2:									; Beginning of if(s1 < i1) is false block (else)
 
 ;			s2 = i1 + s1 + s2;
 	movl	_i1, %eax
@@ -116,10 +121,10 @@ L2:
 	movswl	%ax, %ecx
 	movl	_i2, %edx
 	movl	_i1, %eax
-	movl	%ebx, 16(%esp)
-	movl	%ecx, 12(%esp)
-	movl	%edx, 8(%esp)
-	movl	%eax, 4(%esp)
+	movl	%ebx, 16(%esp)			; push zero extended s2
+	movl	%ecx, 12(%esp)			; push sign extended s1
+	movl	%edx, 8(%esp)			; push i2
+	movl	%eax, 4(%esp)			; push i1
 	movl	$LC0, (%esp)
 	call	_printf
 	
@@ -137,25 +142,31 @@ L1:
 _main:
 	pushl	%ebp
 	movl	%esp, %ebp
-	andl	$-16, %esp
-	subl	$32, %esp
+	andl	$-16, %esp			; −16: 0XFFF0 set the lowest 4 bits to 0
+								; align at 8−byte boundary
+								; $−16 : immediate addressing (constant 16)
+	
+	
+	subl	$32, %esp			; allocate areas for the local variables and
+								; the parameters for the functions to call
+								; 32 = 8 * 4
 	
 ;		void main(int argc, char **argv, char **envp)
 	call	___main
 	
 ;		{
 ;			int m1 = 8;
-	movl	$8, 28(%esp)
+	movl	$8, 28(%esp)		; 28(%esp): m1
 	
 ;			short m2 = 5;
-	movw	$5, 26(%esp)
+	movw	$5, 26(%esp)		; 26(%esp): m2
 	
-;			func(m1, m2, 9);
-	movswl	26(%esp), %eax
-	movl	$9, 8(%esp)
-	movl	%eax, 4(%esp)
-	movl	28(%esp), %eax
-	movl	%eax, (%esp)
+;			func(m1, m2, 9);	
+	movswl	26(%esp), %eax		; 26(%esp): m2
+	movl	$9, 8(%esp)			
+	movl	%eax, 4(%esp)		; push sign extended m2
+	movl	28(%esp), %eax		; 28(%esp): m1
+	movl	%eax, (%esp)		; push m1
 	call	_func
 	leave
 	
