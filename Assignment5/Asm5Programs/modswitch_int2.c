20a06c726085ce1d66f63005df51444ce8f5e63f
BlakeWewer <you@example.com>
2018-04-18 11:31:07 -0500

    Added notes to document Created modswitch_int2.c

---
+++ Assignment5/Asm5Programs/modswitch_int2.c
@@ -0,0 +1,21 @@
+void main() {
+  int x = 4;
+  int i;
+
+  switch (x) {
+   case 10:
+     i += 10;
+     break;
+   case 18:
+     i += 18;
+     break;
+   case 3:
+     i += 3;
+     break;
+   case -4:
+     i -= 4;
+     break;
+   default:
+     i += 1;
+  }
+}
